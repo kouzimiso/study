8b6d8711765bf78804f021f4def6e1048aa0a1f8
味噌 麹 <kouzim3@gmail.com>
2022-11-14 11:09:47 +0900

    Add houchi_auto_ikusei Add test code in TensorFlow

--- Test/TestTensorFlow2.py
+++ Test/TestTensorFlow2.py
@@ -4,7 +4,28 @@ import tensorflow as tf
 # Helper libraries
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.pyplot
 
+#matplotlib.pyplotの動作確認
+x = np.linspace(-3,3)
+print(x)
+y = x**2
+print(y)
+fig = matplotlib.pyplot.figure(
+  figsize = (5,5), 
+  facecolor="lightgray",
+  tight_layout=True,
+  edgecolor="red",
+  linewidth=5
+  )
+axes =fig.add_subplot()
+axes.set_title("subplot") 
+axes.set_xlabel("x-label")
+axes.set_ylabel("y-label")
+
+matplotlib.pyplot.plot(x,y)
+
+matplotlib.pyplot.show()
 
 print(tf.__version__)
 
@@ -23,6 +44,18 @@ print(train_labels)
 
 plt.figure()
 plt.imshow(train_images[0])
+plt.colorbar()
+plt.grid(False)
+plt.show()
+
+plt.figure()
+plt.imshow(train_images[1])
+plt.colorbar()
+plt.grid(False)
+plt.show()
+
+plt.figure()
+plt.imshow(train_images[2])
 plt.colorbar()
 plt.grid(False)
 plt.show()
@@ -108,4 +141,62 @@ plot_image(i, predictions[i], test_labels, test_images)
 plt.subplot(1,2,2)
 plot_value_array(i, predictions[i],  test_labels)
 plt.show()
+
   
+i = 12
+plt.figure(figsize=(6,3))
+plt.subplot(1,2,1)
+plot_image(i, predictions[i], test_labels, test_images)
+plt.subplot(1,2,2)
+plot_value_array(i, predictions[i],  test_labels)
+plt.show()
+
+# Plot the first X test images, their predicted labels, and the true labels.
+# Color correct predictions in blue and incorrect predictions in red.
+num_rows = 5
+num_cols = 3
+num_images = num_rows*num_cols
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_images):
+  plt.subplot(num_rows, 2*num_cols, 2*i+1)
+  plot_image(i, predictions[i], test_labels, test_images)
+  plt.subplot(num_rows, 2*num_cols, 2*i+2)
+  plot_value_array(i, predictions[i], test_labels)
+plt.tight_layout()
+plt.show()
+
+# Plot the first X test images, their predicted labels, and the true labels.
+# Color correct predictions in blue and incorrect predictions in red.
+num_rows = 5
+num_cols = 3
+num_images = num_rows*num_cols
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_images):
+  plt.subplot(num_rows, 2*num_cols, 2*i+1)
+  plot_image(i, predictions[i], test_labels, test_images)
+  plt.subplot(num_rows, 2*num_cols, 2*i+2)
+  plot_value_array(i, predictions[i], test_labels)
+plt.tight_layout()
+plt.show()
+
+
+# Grab an image from the test dataset.
+img = test_images[1]
+
+print(img.shape)
+
+# Add the image to a batch where it's the only member.
+img = (np.expand_dims(img,0))
+
+print(img.shape)
+
+
+predictions_single = probability_model.predict(img)
+
+print(predictions_single)
+
+plot_value_array(1, predictions_single[0], test_labels)
+_ = plt.xticks(range(10), class_names, rotation=45)
+plt.show()
+
+np.argmax(predictions_single[0])
---
+++ houchi_auto_ikusei/ｃ級敏体.bat
@@ -0,0 +1,3 @@
+SET /P STR="ˆç¬‰ñ”F"
+python main.py c 0.01 1 0 1 %STR%
+PAUSE
