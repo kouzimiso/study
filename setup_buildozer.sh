5c47328539b10dcda94ef14d1a1053d036ddf73d
味噌 麹 <kouzim3@gmail.com>
2024-07-26 16:27:59 +0900

    update

--- setup_buildozer.sh
+++ setup_buildozer.sh
@@ -49,21 +49,9 @@ from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
-
 from kivy.core.text import LabelBase, DEFAULT_FONT
 from kivy.utils import platform
-
-# フォントのパスを指定してフォントを設定する
-if platform == 'win':
-    # Windowsの場合はシステムフォントを使用
-    font_path = "C:/Windows/Fonts/YuGothR.ttc"
-    LabelBase.register(DEFAULT_FONT, fn_regular=font_path)
-elif platform == 'android':
-    # Androidの場合はassetsフォルダ内のフォントを使用
-    LabelBase.register(DEFAULT_FONT, fn_regular='assets/fonts-japanese-gothic.ttf')
-else:
-    # その他のプラットフォームではデフォルトフォントを使用
-    LabelBase.register(DEFAULT_FONT, fn_regular='DejaVuSans.ttf')
+import os
 
 class MyApp(App):
     def build(self):
@@ -78,8 +66,34 @@ class MyApp(App):
     def on_button_click(self, instance):
         self.label.text = 'ボタンがクリックされました！'
 
+    def set_custom_font(self):
+        # フォントファイルのパスを設定
+        if platform == 'macosx':
+            # macOSの場合はシステムフォントを使用
+            font_path = "/System/Library/Fonts/ヒラギノ角ゴシック W6.ttc"
+        elif platform == 'win':
+            # Windowsの場合はシステムフォントを使用
+            font_path = "C:/Windows/Fonts/YuGothR.ttc"
+        elif platform == 'android':
+            # Androidの場合はassetsフォルダ内のフォントを使用
+            # assetsフォルダ内のフォントファイルを動的に探す
+            assets_dir = os.path.join(os.path.dirname(__file__), 'assets')
+            font_files = [f for f in os.listdir(assets_dir) if f.endswith('.ttf') or f.endswith('.ttc')]
+            
+            if font_files:
+                font_path = os.path.join(assets_dir, font_files[0])
+                LabelBase.register(DEFAULT_FONT, fn_regular=font_path)
+        else:
+            # その他のプラットフォームではデフォルトフォントを使用
+            font_path = None
+         # フォントを登録
+        if font_path and os.path.exists(font_path):
+            LabelBase.register(DEFAULT_FONT, fn_regular=font_path)
+            
 if __name__ == '__main__':
-    MyApp().run()
+    app = MyApp()
+    app.set_custom_font()
+    app.run()
 EOF
 
 # Buildozerプロジェクトの初期化とビルド
